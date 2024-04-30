d3d2183a41ad3d594c43376cae03fda85fa4699e
Samuel Van Campenhout <samuel.vancampenhout@student.vinci.be>
2024-04-30 18:45:14 +0200

    add management of course

--- db.py
+++ db.py
@@ -9,5 +9,26 @@ class User(db.Model):
     name = db.Column(db.String, nullable=True)
     first_name = db.Column(db.String, nullable=True)
     email = db.Column(db.String, nullable=True)
+    organization_code = db.Column(db.String, nullable=True)
     admin = db.Column(db.Boolean, default=False)
 
+class Course(db.Model):
+    __tablename__ = 'course'
+    id = Column(Integer, primary_key=True)
+    code = Column(String)
+    organization_code = Column(String)
+    title = Column(String)
+    quadri = Column(Integer)
+    year = Column(String)
+    load_needed = Column(Integer)
+
+# Ajouter le premier admin dans la base de données
+def add_first_admin():
+    session = Session()
+    admin_exists = session.query(User).filter_by(admin=True).first()
+    if admin_exists is None:
+        first_admin = User(name='Admin', first_name='Admin', email='admin@example.com', organization_code="", admin=True)
+        session.add(first_admin)
+        session.commit()
+
+add_first_admin()

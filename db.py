ad4c367896d4a37245e20d7e55906c264b488115
Samuel Van Campenhout <samuel.vancampenhout@student.vinci.be>
2024-05-16 16:13:09 +0200

    - add a user type + supervisor if it's a researcher - language added to course table - added researcher / teacher  / assigned user preferences tables

--- db.py
+++ db.py
@@ -11,24 +11,64 @@ class User(db.Model):
     email = db.Column(db.String, nullable=True)
     organization_code = db.Column(db.String, nullable=True)
     admin = db.Column(db.Boolean, default=False)
+    is_teacher = db.Column(db.Boolean, default=False)
+    is_researcher = db.Column(db.Boolean, default=False)
+    supervisor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    supervisor = db.relationship('User', remote_side=[id], backref='supervisees')
+
 
 class Course(db.Model):
     __tablename__ = 'course'
-    id = Column(Integer, primary_key=True)
-    code = Column(String)
-    organization_code = Column(String)
-    title = Column(String)
-    quadri = Column(Integer)
-    year = Column(String)
-    load_needed = Column(Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.String, primary_key=True)
+    code = db.Column(db.String)
+    organization_code = db.Column(db.String)
+    title = db.Column(db.String)
+    quadri = db.Column(db.Integer)
+    load_needed = db.Column(db.Integer, default=0)
+    language = db.Column(db.String)
 
-# Ajouter le premier admin dans la base de données
-def add_first_admin():
-    session = Session()
-    admin_exists = session.query(User).filter_by(admin=True).first()
-    if admin_exists is None:
-        first_admin = User(name='Admin', first_name='Admin', email='admin@example.com', organization_code="", admin=True)
-        session.add(first_admin)
-        session.commit()
 
-add_first_admin()
+class Researcher(db.Model):
+    __tablename__ = 'researcher'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    research_field = db.Column(db.String)
+    active = db.Column(db.Boolean, default=True)
+    max_loads = db.Column(db.Integer)
+    jokers = db.Column(db.Integer)
+    researcher_type = db.Column(db.String)
+
+    user = db.relationship('User', backref=db.backref('user_researcher', lazy=True))
+
+
+class Teacher(db.Model):
+    __tablename__ = 'teacher'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    course_id = db.Column(db.Integer, nullable=False)
+    course_year = db.Column(db.String, nullable=False)
+
+    # Creation of a link to the compound key (id, year) of the course table
+    __table_args__ = (
+        db.ForeignKeyConstraint(
+            ['course_id', 'course_year'],
+            ['course.id', 'course.year']
+        ),
+    )
+
+    user = db.relationship('User', backref=db.backref('user_teacher', lazy=True))
+    course = db.relationship('Course', backref=db.backref('course_teacher', lazy=True))
+
+
+class PreferenceAssignment(db.Model):
+    __tablename__ = 'preference_assignment'
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.String, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    researcher_id = db.Column(db.Integer, db.ForeignKey('researcher.id'), nullable=False)
+
+    course = db.relationship('Course', backref=db.backref('course_preference_assignment', lazy=True))
+    researcher = db.relationship('Researcher', backref=db.backref('researcher_preference_assignment', lazy=True))
+

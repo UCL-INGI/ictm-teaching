f2a2ef742e78d6d901121169da98643f85f6c58d
Samuel Van Campenhout <grami474@gmail.com>
2024-07-06 21:57:42 +0200

    use JS utility script when common functions

--- static/scripts/util.js
+++ static/scripts/util.js
@@ -13,7 +13,7 @@ function sendPostRequest(url, data, successCallback, errorCallback) {
 
 // Function to handle the change event of the yearSelect dropdown
 function handleYearSelectChange(params) {
-    $('#yearSelect').change(function() {
+    $('#yearSelect').change(function () {
         params.selected_year = $(this).val();
         let updatedUrl = "/config/update_current_year";
 
@@ -23,20 +23,66 @@ function handleYearSelectChange(params) {
         sendPostRequest(
             updatedUrl,
             params,
-            function(response) {
+            function (response) {
                 window.location.href = response;
             },
-            function(error) {
+            function (error) {
                 console.error('Error updating the year:', error);
             }
         );
     });
 }
 
-// Initialization function to set up event handlers
-function initializeHandlers() {
-    handleYearSelectChange();
+function addBadge(selectElement, containerSelector) {
+    var selectedOption = $(selectElement).find('option:selected');
+    var selectedValue = selectedOption.val();
+    var selectedText = selectedOption.text();
+
+    console.log("Selected value:", selectedValue);
+    console.log("Selected text:", selectedText);
+
+    if (selectedValue) {
+        // Check if the organization is already added
+
+        var alreadyExists = $(containerSelector).find('div[data-id="' + selectedValue + '"]').length > 0;
+
+        console.log("Already exists:", alreadyExists)
+
+        if (!alreadyExists) {
+            var tagHtml = $('<div>', {
+                class: 'badge bg-primary d-inline-block',
+                'data-id': selectedValue
+            });
+
+            $(tagHtml).text(selectedText);
+            $('<a>', {
+                href: '#',
+                class: 'remove-tag ms-1',
+                html: '&times;'
+            }).appendTo(tagHtml);
+            $('<input>', {
+                type: 'hidden',
+                name: 'organization_code[]',
+                value: selectedValue
+            }).appendTo(tagHtml);
+
+            $(containerSelector).append(tagHtml);
+            console.log("Badge added:", tagHtml);
+        } else {
+            console.log("Organization already added:", selectedText);
+        }
+
+        // Reset the select input
+        $(selectElement).val('');
+
+        console.log("Container content after adding badge:", $(containerSelector).html());
+    }
 }
 
-// Expose the initializeHandlers function to be called from HTML
-// window.initializeHandlers = initializeHandlers;
+// Event listener for removing the organization tag
+$(document).on('click', '.remove-tag', function (e) {
+    e.preventDefault();
+    $(this).parent('.badge').remove();
+    console.log("Tag removed");
+});
+

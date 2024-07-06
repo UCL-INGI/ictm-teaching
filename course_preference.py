f56c7f415397f631b4d81e41c1d81ad58ca746f3
Samuel Van Campenhout <grami474@gmail.com>
2024-07-06 17:23:00 +0200

    Use JSON encoded dict correctly

--- course_preference.py
+++ course_preference.py
@@ -14,7 +14,7 @@ def delete_old_preferences(researcher_id, course_ids, current_year):
         db.session.query(PreferenceAssignment).filter(
             PreferenceAssignment.researcher_id == researcher_id,
             PreferenceAssignment.course_year == current_year,
-            ~PreferenceAssignment.course_id.in_(course_ids)
+            PreferenceAssignment.course_id.in_(course_ids)
         ).delete()
         db.session.commit()
     except Exception as e:
@@ -40,24 +40,17 @@ def save_preference():
     if researcher is None:
         return make_response("User is not a researcher", 500)
 
-    new_course_ids = {preference.split("-", 1)[0] for preference in preferences}
-    test_ids = {preference['course_id'] for preference in preferences}
+    new_course_ids = {preference['course_id'] for preference in preferences}
     delete_old_preferences(researcher.id, new_course_ids, current_year)
 
     for preference in preferences:
         try:
-            course_id, course_year = preference.split("-", 1)
-            existing_preference = db.session.query(PreferenceAssignment).filter_by(
-                course_id=course_id,
-                course_year=course_year,
-                researcher_id=researcher.id
-            ).first()
-
-            if existing_preference is None:
-                new_preference = PreferenceAssignment(course_id=course_id, course_year=course_year,
-                                                      researcher_id=researcher.id)
-                db.session.add(new_preference)
-                db.session.commit()
+            course_id = preference['course_id']
+            course_year = preference['course_year']
+            new_preference = PreferenceAssignment(course_id=course_id, course_year=course_year,
+                                                  researcher_id=researcher.id)
+            db.session.add(new_preference)
+            db.session.commit()
         except Exception as e:
             db.session.rollback()
             raise e

ff5ecc37f46ab0becdb9bfbce933fc45a9be3c64
Samuel Van Campenhout <grami474@gmail.com>
2024-07-29 22:56:49 +0200

    Improve if condition to check all cases

--- decorators.py
+++ decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import session, redirect, url_for, flash
-from db import db, User
+from db import db, User, Role
 import auth
 
 
@@ -22,7 +22,7 @@ def check_access_level(access_level):
         def decorated_function(*args, **kwargs):
             user_id_profile = kwargs.get('user_id')
             user = db.session.query(User).get(session["user_id"])
-            if not user.allowed(access_level) or (user_id_profile and user.id != user_id_profile):
+            if not user.allowed(access_level) or (user_id_profile and user.id != user_id_profile and not user.allowed(Role.ADMIN)):
                 flash("Permission denied. You do not have access to this page.", "error")
                 return redirect(url_for("index"))
             else:

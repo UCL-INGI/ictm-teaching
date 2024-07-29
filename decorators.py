5593c9f8ba5167b8c4e2c4facaa929ecdfdec148
Samuel Van Campenhout <grami474@gmail.com>
2024-07-30 01:20:59 +0200

    Add possibility to put several roles in check access and dont allow a user to access a profile that is not his own

--- decorators.py
+++ decorators.py
@@ -16,17 +16,24 @@ def login_required(f):
     return decorated_function
 
 
-def check_access_level(access_level):
+def check_access_level(*allowed_roles):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             user_id_profile = kwargs.get('user_id')
             user = db.session.query(User).get(session["user_id"])
-            if not user.allowed(access_level) or (user_id_profile and user.id != user_id_profile and not user.allowed(Role.ADMIN)):
+
+            # Check if the user has not the required role
+            if not any(user.allowed(role) for role in allowed_roles):
                 flash("Permission denied. You do not have access to this page.", "error")
                 return redirect(url_for("index"))
-            else:
-                return f(*args, **kwargs)
+
+            # Check if the user is trying to access a profile that is not his own
+            if user_id_profile and user.id != user_id_profile and not user.allowed(Role.ADMIN):
+                flash("Permission denied. You do not have access to this page.", "error")
+                return redirect(url_for("index"))
+
+            return f(*args, **kwargs)
 
         return decorated_function
 

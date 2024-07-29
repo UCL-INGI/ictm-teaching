6ad7d39bbdb96cbc99a61dc0d3178d532ca21a15
Samuel Van Campenhout <grami474@gmail.com>
2024-07-29 21:37:38 +0200

    add a check to ensure only administrators can edit other profiles.

--- decorators.py
+++ decorators.py
@@ -20,8 +20,9 @@ def check_access_level(access_level):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
+            user_id_profile = kwargs.get('user_id')
             user = db.session.query(User).get(session["user_id"])
-            if not user.allowed(access_level):
+            if not user.allowed(access_level) or (user_id_profile and user.id != user_id_profile):
                 flash("Permission denied. You do not have access to this page.", "error")
                 return redirect(url_for("index"))
             else:

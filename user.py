56cfd84b897c70e5fc25fb7396c024e7a106ad7b
Samuel Van Campenhout <162597817+SamuelVch98@users.noreply.github.com>
2024-09-09 10:05:50 +0200

    Fix #12 : no permission check on profile edition (#17)

--- user.py
+++ user.py
@@ -94,9 +94,18 @@ def users(user_type):
     return render_template('users.html', users=all_users, user_type=user_type)
 
 
+def is_allowed_user(user_id):
+    return user_id == session["user_id"] or session["is_admin"]
+
+
 @user_bp.route('/profile/<int:user_id>/<int:current_year>')
 @login_required
+@check_access_level(Role.ADMIN, Role.RESEARCHER, Role.TEACHER)
 def user_profile(user_id, current_year):
+    if not is_allowed_user(user_id):
+        flash("Permission denied. You do not have access to this page.", "error")
+        return redirect(url_for("index"))
+
     all_users = db.session.query(User).filter(User.admin == False, User.is_teacher == True, User.active == True).all()
     requested_user = db.session.query(User).filter_by(id=user_id).first()
     researcher = db.session.query(Researcher).filter(Researcher.user_id == requested_user.id).first()
@@ -117,12 +126,17 @@ def user_profile(user_id, current_year):
                            current_year=current_year)
 
 
-@user_bp.route('/update_user_profile', methods=['POST'])
+@user_bp.route('/update_user_profile/<int:user_id>', methods=['POST'])
 @login_required
-def update_user_profile():
+@check_access_level(Role.ADMIN, Role.RESEARCHER, Role.TEACHER)
+def update_user_profile(user_id):
     form = request.form
     if not form:
         return make_response("Problem with form request", 500)
+
+    if not is_allowed_user(user_id):
+        flash("Permission denied. You do not have access to this page.", "error")
+        return redirect(url_for("index"))
 
     name = request.form['name']
     first_name = request.form['first_name']
@@ -136,7 +150,6 @@ def update_user_profile():
     if email != '' and not is_valid_email(email):
         return make_response("Invalid email format", 400)
 
-    user_id = request.form['user_id']
     organization_code = None if request.form['organization_code'] == 'None' else request.form['organization_code']
     is_teacher = True if 'is_teacher' in request.form else False
     is_researcher = True if 'is_researcher' in request.form else False
